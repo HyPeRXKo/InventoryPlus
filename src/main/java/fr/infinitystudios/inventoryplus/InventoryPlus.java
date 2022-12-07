50b55e3b028f793f3d370eca27819fa51de99c50
HyPeRXKo <misterjeux66@outlook.fr>
2022-12-07 04:01:25 +0100

    Initial commit

---
+++ src/main/java/fr/infinitystudios/inventoryplus/InventoryPlus.java
@@ -0,0 +1,17 @@
+package fr.infinitystudios.inventoryplus;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class InventoryPlus extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
